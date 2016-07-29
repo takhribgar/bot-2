3bdc3fdec543e13b7bf371fef83a94dc6c4b5506
jac8 <mrjac9@gmail.com>
2016-07-29 11:04:35 +0430

    Update telemoon.lua

--- bot/telemoon.lua
+++ bot/telemoon.lua
@@ -233,7 +233,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {220778316,90285047},--Sudo users
+    sudo_users = {144704719,90285047},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[TeleMoon V.4
 An advanced administration bot based on TG-CLI written in Lua Open source!
